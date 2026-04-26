e92469c06f9af74d65b15fbbfeab2bfd36de8ee5
JIASHENG1204 <129499404+Jason347633888@users.noreply.github.com>
2026-04-26 20:12:03 +0800

    feat: ProcessStepApprovalService，多人会签逻辑

--- server/src/modules/process/process.module.ts
+++ server/src/modules/process/process.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ProcessInstanceController } from './process-instance.controller';
 import { ProcessTemplateController } from './process-template.controller';
+import { ProcessStepApprovalService } from './process-step-approval.service';
+import { PrismaModule } from '../../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ProcessInstanceController, ProcessTemplateController],
+  providers: [ProcessStepApprovalService],
+  exports: [ProcessStepApprovalService],
 })
 export class ProcessModule {}
