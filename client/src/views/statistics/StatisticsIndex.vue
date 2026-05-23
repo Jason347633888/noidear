2e4392171cd51eeb83f32da0ec472ae6dca79a6a
JIASHENG1204 <129499404+Jason347633888@users.noreply.github.com>
2026-05-23 15:36:17 +0800

    feat: delete statistics center views and API adapter

--- client/src/views/statistics/StatisticsIndex.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="statistics-index">
-    <PageHeaderBlock eyebrow="数据分析" title="统计中心" />
-    <el-empty description="请选择统计模块" />
-  </div>
-</template>
-
-<script setup lang="ts">
-</script>
